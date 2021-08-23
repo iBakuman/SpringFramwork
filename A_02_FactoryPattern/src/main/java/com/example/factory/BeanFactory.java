144eb0b67686ceabda8f9088ae0752cc3e2f167c
iBakuman <903926441@qq.com>
2021-08-23 19:01:17 +0800

    A_02_FactoryPattern     Chinese:         1.使用单例模式优化性能     English:         1.Optimize performance using the singleton mode

--- A_02_FactoryPattern/src/main/java/com/example/factory/BeanFactory.java
+++ A_02_FactoryPattern/src/main/java/com/example/factory/BeanFactory.java
@@ -1,6 +1,9 @@
 package com.example.factory;
 
 import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -13,30 +16,56 @@ import java.util.Properties;
 public class BeanFactory {
     private static Properties props;
 
+    // 定义一个Map，用于存放我们要创建的对象
+    private static Map<String, Object> beans;
+
     static {
         try {
             props = new Properties();
             InputStream in = BeanFactory.class.getClassLoader().getResourceAsStream("bean.properties");
             props.load(in);
+            // 实例化容器
+            beans = new HashMap<String, Object>();
+            // 去除配置文件的所有key
+            Enumeration<Object> keys = props.keys();
+            while (keys.hasMoreElements()) {
+                // 去除每个key
+                String key = keys.nextElement().toString();
+                // 根据key获取value
+                String beanPath = props.getProperty(key);
+                // 反射创建对象
+                Object value = Class.forName(beanPath).newInstance();
+                // 放入容器
+                beans.put(key, value);
+            }
         } catch (Exception e) {
             throw new ExceptionInInitializerError("初始化properties失败!");
         }
     }
 
     /**
-     * 根据bean的名称获取bean对象
-     *
+     * 根据bean的名称获取对象，单例模式
      * @param beanName
      * @return
      */
-    public  static Object getBean(String beanName) {
-        Object bean = null;
-        try {
-            String beanPath = props.getProperty(beanName);
-            bean = Class.forName(beanPath).newInstance();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return bean;
+    public static Object getBean(String beanName) {
+        return  beans.get(beanName);
     }
+
+    ///**
+    // * 根据bean的名称获取bean对象
+    // *
+    // * @param beanName
+    // * @return
+    // */
+    //public  static Object getBean(String beanName) {
+    //    Object bean = null;
+    //    try {
+    //        String beanPath = props.getProperty(beanName);
+    //        bean = Class.forName(beanPath).newInstance();
+    //    } catch (Exception e) {
+    //        e.printStackTrace();
+    //    }
+    //    return bean;
+    //}
 }
--- A_02_FactoryPattern/src/main/java/com/example/ui/Client.java
+++ A_02_FactoryPattern/src/main/java/com/example/ui/Client.java
@@ -1,14 +1,17 @@
 package com.example.ui;
 
+import com.example.factory.BeanFactory;
 import com.example.service.IAccountService;
-import com.example.service.impl.AccountServiceImpl;
 
 /**
  * 模拟表现层，用于调用业务层
  */
 public class Client {
     public static void main(String[] args) {
-        IAccountService service = new AccountServiceImpl();
-        service.saveAccount();
+        for (int i = 0; i < 5; ++i) {
+            IAccountService service = (IAccountService) BeanFactory.getBean("accountService");
+            System.out.println(service);
+        }
+        //service.saveAccount();
     }
 }

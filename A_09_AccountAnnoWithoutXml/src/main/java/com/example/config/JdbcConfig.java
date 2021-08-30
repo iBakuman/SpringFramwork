d2a660847a9eb9be4cc45a4a55f65e984d4c24ba
iBakuman <903926441@qq.com>
2021-08-30 20:12:05 +0800

    A_09_AccountAnnoWithoutXml     Chinese:         1.通过注解指定Spring多个配置类的第一种方式：           1.使用AnnotationConfigApplicationContext创建容器时传入配置类的字节码来指定主配置类           2.在主配置类中使用@ComponentSan来指定其他副配置类所在包           3.在副配置类中使用@Configuration指定该类是一个配置类(主配置类可以不写@Configuration)     English:         1.The first way to specify Spring's multiple configuration           classes via annotations         2.Use the class AnnotationConfigApplicationContext create           container by passing in the configuration class's bytecode to           specify the configuration class         3.Use @ComponentScan in the main configuration class to specify           the package for other sub-configuration classes         4.Use @configuration in the secondary Configuration class to           specify that the class is a configuration class

---
+++ A_09_AccountAnnoWithoutXml/src/main/java/com/example/config/JdbcConfig.java
@@ -0,0 +1,46 @@
+package com.example.config;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.apache.commons.dbutils.QueryRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * @author Avarice
+ * @date 2021/8/30 20:00
+ */
+@Configuration
+public class JdbcConfig {
+    /**
+     * 用于创建一个QueryRunner对象
+     *
+     * @param ds
+     * @return
+     */
+    @Bean(name = "runner")
+    //@Scope("prototype")// 设置为多例对象
+    public QueryRunner createQueryRunner(DataSource ds) {
+        return new QueryRunner(ds);
+    }
+
+    /**
+     * 创建数据源对象
+     *
+     * @return
+     */
+    @Bean(name = "ds")
+    public DataSource createDataSource() {
+        try {
+            ComboPooledDataSource ds = new ComboPooledDataSource();
+            ds.setDriverClass("com.mysql.jdbc.Driver");
+            ds.setJdbcUrl("jdbc:mysql://localhost:3306/test");
+            ds.setUser("root");
+            ds.setPassword("193196");
+            return ds;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- A_09_AccountAnnoWithoutXml/src/main/java/com/example/config/SpringConfig.java
+++ A_09_AccountAnnoWithoutXml/src/main/java/com/example/config/SpringConfig.java
@@ -1,12 +1,7 @@
 package com.example.config;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
-import org.apache.commons.dbutils.QueryRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 /**
  * 该类是一个配置类，和bean.xml的作用是一样的
@@ -30,38 +25,8 @@ import javax.sql.DataSource;
  */
 
 @Configuration
-@ComponentScan("com.example")
+@ComponentScan({"com.example"})
 //@ComponentScan(basePackages = {"com.example"}) // 作用同上一行的注解一样
 public class SpringConfig {
 
-    /**
-     * 用于创建一个QueryRunner对象
-     *
-     * @param ds
-     * @return
-     */
-    @Bean(name = "runner")
-    //@Scope("prototype")// 设置为多例对象
-    public QueryRunner createQueryRunner(DataSource ds) {
-        return new QueryRunner(ds);
-    }
-
-    /**
-     * 创建数据源对象
-     *
-     * @return
-     */
-    @Bean(name = "ds")
-    public DataSource createDataSource() {
-        try {
-            ComboPooledDataSource ds = new ComboPooledDataSource();
-            ds.setDriverClass("com.mysql.jdbc.Driver");
-            ds.setJdbcUrl("jdbc:mysql://localhost:3306/test");
-            ds.setUser("root");
-            ds.setPassword("193196");
-            return ds;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 }

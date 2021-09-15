266b1565b1bccfaf7c1cedb2e67f70f52795a891
iBakuman <903926441@qq.com>
2021-09-15 21:33:30 +0800

    A_18_AccountAopAnno     Chinese:         1.使用环绕通知来解决上一个转账实现出现的问题     English:         1.Use surround notifaction to solve the problem with the           previous transfer implementation

--- A_18_AccountAopAnno/src/main/java/com/example/utils/TransactionManager.java
+++ A_18_AccountAopAnno/src/main/java/com/example/utils/TransactionManager.java
@@ -1,5 +1,6 @@
 package com.example.utils;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -8,6 +9,7 @@ import java.sql.SQLException;
 
 /**
  * 和事务管理相关的工具类，它包含了：开启事务、提交事务、回滚事务和释放连接
+ *
  * @author Avarice
  * @date 2021/9/5 19:06
  */
@@ -19,7 +21,8 @@ public class TransactionManager {
     private ConnectionUtils connectionUtils;
 
     @Pointcut("execution(* com.example.service.impl.*.*(..))")
-    private void pt1(){}
+    private void pt1() {
+    }
 
     public ConnectionUtils getConnectionUtils() {
         return connectionUtils;
@@ -28,7 +31,7 @@ public class TransactionManager {
     /**
      * 开启事务
      */
-    @Before("pt1()")
+    //@Before("pt1()")
     public void beginTransaction() {
         try {
             connectionUtils.getThreadConnection().setAutoCommit(false);
@@ -40,7 +43,7 @@ public class TransactionManager {
     /**
      * 提交事务
      */
-    @AfterReturning("pt1()")
+    //@AfterReturning("pt1()")
     public void commit() {
         try {
             connectionUtils.getThreadConnection().commit();
@@ -52,7 +55,7 @@ public class TransactionManager {
     /**
      * 回滚事务
      */
-    @AfterThrowing("pt1()")
+    //@AfterThrowing("pt1()")
     public void rollback() {
         try {
             connectionUtils.getThreadConnection().rollback();
@@ -64,13 +67,40 @@ public class TransactionManager {
     /**
      * 释放连接
      */
-    @After("pt1()")
+    //@After("pt1()")
     public void release() {
         try {
             connectionUtils.getThreadConnection().close();// 先关闭连接
             connectionUtils.removeConnection();// 后解除线程和连接的绑定
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Around("pt1()")
+    public Object aroundAdvice(ProceedingJoinPoint pjp) {
+        Object rtValue = null;
+        try {
+            // 1.获取方法参数
+            Object args[] = pjp.getArgs();
+
+            // 2.开启事务
+            this.beginTransaction();
+
+            // 3.执行方法
+            rtValue = pjp.proceed(args);
+
+            // 4.提交事务
+            this.commit();
+
+            return rtValue;
+        } catch (Throwable e) {
+            // 5.回滚事务
+            this.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            // 6.释放资源
+            this.release();
         }
     }
 }
